caef8a98653c5d556dca1aacfe91de047d36e78b
Francis-Bui <witzp3ych0@gmail.com>
2025-05-21 15:03:15 -0700

    rebuild

--- templates/assets/data-BELRId44.js
+++
@@ -1,1 +0,0 @@
-import{ah as e,S as a,U as s,W as r,a2 as n}from"./index-CX1j4QnU.js";import{V as o}from"./VContainer-CI_0DmAy.js";const c={};function f(i,t){return s(),a(o,{class:"fill-height d-flex align-center justify-center"},{default:r(()=>t[0]||(t[0]=[n("span",{class:"text-h4 font-weight-bold"},"Data Page",-1)])),_:1})}const _=e(c,[["render",f]]);export{_ as default};
